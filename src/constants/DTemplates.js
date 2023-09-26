f76928d29339c1ed7b6e2f11cf871b58a379c627
Ulises Baroni <ulisesbaroni@gmail.com>
2023-09-26 19:59:52 -0300

    Correción de tipeo

--- src/constants/DTemplates.js
+++ src/constants/DTemplates.js
@@ -2,5 +2,5 @@ export default {
     HOLA: "hola",
     RESTORE: "restore",
     PRODUCTOELIMINADO: "removedProduct",
-    CONFIRMACIONCOMPRA: "confirmedpurchase",
+    CONFIRMACIONCOMPRA: "confirmedPurchase",
   };
